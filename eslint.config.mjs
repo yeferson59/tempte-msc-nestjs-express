eaa711fbe897cd349466b395cc4cc366588d422d
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-31 23:23:47 -0500

    chore: update to modules

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import eslintPlugin from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaVersion: 2022,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPlugin,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+    ignores: ['dist', 'node_modules', 'coverage'],
+  },
+];
